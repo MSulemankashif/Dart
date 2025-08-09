13abab227848c5798e42b41b2906badc2f561939
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-09 16:59:06 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,25 @@
 void main(){
   print("Hello World");
 
-  String name = 'Suleman';
-  int age = 10;
-  double height = 5.5;
+  // String name = 'Suleman';
+  // int age = 10;
+  // double height = 5.5;
 
-  print(name);
-  print(age);
-  print(height);
+  // print(name);
+  // print(age);
+  // print(height);
+
+  int a = 10;
+  int b = 20;
+
+  // print(a+b);
+
+  int age = 18;
+  if(age >= 18){
+    print("You are an adult");
+  }else{
+    print("You are not an adult");
+  }
 
   
 }
