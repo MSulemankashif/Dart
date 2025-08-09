4a9ce7a68e457bb9000e7db803e0f63ca2e1a26a
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-09 17:31:21 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,18 +1,18 @@
 void main(){
   print("Hello World");
 
-  // String name = 'Suleman';
-  // int age = 10;
-  // double height = 5.5;
+  /*String name = 'Suleman';
+  int age = 10;
+  double height = 5.5;
 
-  // print(name);
-  // print(age);
-  // print(height);
+  print(name);
+  print(age);
+  print(height);
 
   int a = 10;
   int b = 20;
 
-  // print(a+b);
+  print(a+b);
 
   int age = 18;
   if(age >= 18){
@@ -23,5 +23,27 @@ void main(){
 
   for(int i=0; i<=10; i++){
     print(i);
-  }
+    print("Number $i");
+  } 
+
+ "Functions in Dart"
+
+String name = "Suleman";
+void sayHello(){
+  print('Hello $name');
+};
+
+
+void greetUser (String namee, int age){
+  print("Hello $namee your age is $age");
+};
+
+greetUser("Suleman",20);
+*/
+
+int addNumbers(int a, int b){
+  return a+b;
+}
+int result = addNumbers(10, 20);
+print(result);
 }
