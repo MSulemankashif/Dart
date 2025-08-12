b1fd34abeab825efd0a36a7b442f1e00df641a56
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-12 18:34:05 +0500

    Update OOP.dart

--- OOP.dart
+++ OOP.dart
@@ -1,15 +1,20 @@
 class bike{
-  int Tyres = ;
-  int model = ;
+  int Tyres = 2;
+  int model = 0;
 
+// late  int Tyres;
+// late  int model;
+
+
+// Method (Function Inside a class called Method)
   displayInfo(){
-    print(' Bike has $Tyres Tyres and model of $model');
+    print('This Bike has $Tyres Tyres and model of $model');
   }
 }
 
 void main(){
   bike bike1 = bike();
-  bike1.Tyres = 2;
+  // bike1.Tyres = 2;
   bike1.model = 2020;
   bike1.displayInfo();
 }
