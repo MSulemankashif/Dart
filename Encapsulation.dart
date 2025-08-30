17bff80647e80bf4b74d0fcc88210ea54763c132
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-08-30 18:46:34 +0500

    Practice

--- Encapsulation.dart
+++ Encapsulation.dart
@@ -1,19 +1,17 @@
-class bankAccount {
+class bankAccount{
   double _balance = 0.0;
 
-  // Method to Deposit Money
-  void deposit(double amount) {
-    if (amount > 0) {
+  void deposit(double amount){
+    if(amount > 0){
       _balance += amount;
-      // _balance = _balance + amount;
       print("Deposited: $amount");
     }else{
       print("Invalid Deposit Amount");
     }
   }
 
-  // Method to Withdraw Money
-  void withdraw(double amount) {
+  // Method to withdraw money
+  void withdraw(double amount){
     if(amount <= _balance){
       _balance -= amount;
       print("Withdrew: $amount");
@@ -21,7 +19,8 @@ class bankAccount {
       print("Insufficient Balance");
     }
   }
-  getBalance(){
+
+  double getBalance(){
     return _balance;
   }
 }
@@ -30,6 +29,5 @@ void main(){
   bankAccount account = bankAccount();
   account.deposit(45000);
   account.withdraw(15000);
-
-  print("Your Balance is: ${account.getBalance()}");
+  print('Current Balance: ${account.getBalance()}');
 }
